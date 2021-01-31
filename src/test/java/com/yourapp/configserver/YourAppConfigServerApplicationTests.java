534ff3f024ce8186d37969a1c4bd43f86a3c9844
NEXTEDUCATION\jagadeswaram <NEXTEDUCATION\jagadeswaram@next-hyd-4998.nexteducation.com>
2021-01-31 19:32:06 +0530

    Test/First Commit & Push from Eclipse to Git

---
+++ src/test/java/com/yourapp/configserver/YourAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourapp.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
